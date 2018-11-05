9579413c1b27ddcd18e9d84d4d309f3fbf197aa5
Artur Kapela <artur-k@post.pl>
2018-11-05 20:06:14 +0100

    Initial commit with Zadanie 1

---
+++ zadanie1.py
@@ -0,0 +1,21 @@
+# Zadanie 1 - suma kontrolna z pliku
+file_path = "input.txt"
+
+
+def get_checksum_for_row(row):
+    tmp = list(map(int, row))
+    return max(tmp) - min(tmp)
+
+
+try:
+    rows_checksum = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            row = line.replace('\n', '').split('\t')
+            rows_checksum.append(get_checksum_for_row(row))
+    print('Suma kontrolna pliku {} wynosi {}'.format(file_path, sum(rows_checksum)))
+
+except FileNotFoundError as e:
+    print("Podany plik nie istnieje!", e)
+except Exception as e:
+    print("Wystąpił błąd.", e)
